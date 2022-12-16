dbe5c3b8dc9d87893d85ed32f0227e3b375a903e
Paul Seyfert <pseyfert.mathphys@gmail.com>
2022-12-16 19:40:05 +0100

    part 1 of day 11 (todo: non-move version)

--- 11/c++-jit/main.cpp
+++ 11/c++-jit/main.cpp
@@ -1,3 +1,4 @@
+#undef FMT_CONSTEVAL
 #include <cctype>
 #include <cstdio>
 #include <dlfcn.h>
@@ -6,29 +7,43 @@
 #include <fstream>
 #include <immintrin.h>
 #include <iostream>
-#include <regex>
-#include <stdlib.h>
-#include <type_traits>
-// #include <range/v3/algorithm/equal.hpp>
-// #include <range/v3/numeric/accumulate.hpp>
+#include <range/v3/algorithm/for_each.hpp>
 #include <range/v3/range/conversion.hpp>
-// #include <range/v3/view/any_view.hpp>
 #include <range/v3/view/chunk_by.hpp>
-// #include <range/v3/view/concat.hpp>
-// #include <range/v3/view/drop.hpp>
-// #include <range/v3/view/enumerate.hpp>
-// #include <range/v3/view/exclusive_scan.hpp>
+#include <range/v3/view/drop.hpp>
+#include <range/v3/view/drop_while.hpp>
+#include <range/v3/view/enumerate.hpp>
 #include <range/v3/view/filter.hpp>
 #include <range/v3/view/getlines.hpp>
-// #include <range/v3/view/cache1.hpp>
 #include <range/v3/view/join.hpp>
+#include <range/v3/view/split.hpp>
+#include <range/v3/view/transform.hpp>
+#include <regex>
+#include <stdlib.h>
+#include <string>
+#include <type_traits>
+#include "monkey.h"
+#include "toi.h"
+
+// #include <range/v3/view/cache1.hpp>
 // #include <range/v3/view/repeat_n.hpp>
 // #include <range/v3/view/take.hpp>
-#include <range/v3/algorithm/for_each.hpp>
-#include <range/v3/view/transform.hpp>
-// #include <string>
 // #include "aoc_ffi_ocr.h"
 // #include "aoc_utils/to.hpp"
+// #include <range/v3/algorithm/equal.hpp>
+// #include <range/v3/numeric/accumulate.hpp>
+// #include <range/v3/view/any_view.hpp>
+// #include <range/v3/view/concat.hpp>
+// #include <range/v3/view/drop.hpp>
+// #include <range/v3/view/exclusive_scan.hpp>
+
+template <typename R>
+auto initial_state(R&& init_line) {
+  return int_range(
+      init_line | ranges::view::filter([](auto c) { return c != ' '; }) |
+      ranges::view::drop_while([](auto c) { return c != ':'; }) |
+      ranges::view::drop(1) | ranges::view::split(','));
+}
 
 int main(int argc, char** argv) {
   const std::filesystem::path& in_path{argv[1]};
@@ -37,11 +52,12 @@ int main(int argc, char** argv) {
   auto in_buffer = ranges::getlines_view(instream) | ranges::to_vector;
 
   std::vector<std::size_t> monkey_ids;
+  std::vector<Monkey> monkeys;
 
   ranges::for_each(
       ranges::view::chunk_by(
           in_buffer, [](const auto, const auto b) { return !b.empty(); }),
-      [&monkey_ids](auto monkey) {
+      [&monkey_ids, &monkeys](auto monkey) {
         auto cleaned = monkey | ranges::view::filter(
                                     [](auto line) { return !line.empty(); });
 
@@ -67,6 +83,7 @@ int main(int argc, char** argv) {
         std::FILE* of =
             std::fopen(fmt::format("monkeys/monkey{}.cpp", mon).c_str(), "w");
         // clang-format off
+        fmt::print(of, "#define Vc_NO_VERSION_CHECK\n");
         fmt::print(of, "#include <Vc/Vc>\n");
         fmt::print(of, "\n");
         fmt::print(of, "std::tuple<__m256i, __m256i, std::size_t, std::size_t> Monkey{} (__m256i in) {{\n", mon);
@@ -79,6 +96,17 @@ int main(int argc, char** argv) {
         // clang-format on
         std::fclose(of);
         monkey_ids.push_back(static_cast<std::size_t>(mon - '0'));
+
+        Monkey& this_monkey = monkeys.emplace_back();
+        Vc::AVX2::int_v buffer;
+        std::size_t n = 0;
+        for (auto [i, v] :
+             ranges::view::enumerate(initial_state(starting_setup))) {
+          buffer[i] = v;
+          n++;
+        }
+        this_monkey.items.emplace_back(buffer.data());
+        this_monkey.actual_size = n;
       });
 
   auto make_targets = monkey_ids | ranges::view::transform([](auto id) {
@@ -97,11 +125,53 @@ int main(int argc, char** argv) {
     return retval;
   }
 
-  void* monkey_lib = dlopen("libmonkeys.so", RTLD_NOW);
-  using monkey_ptr =
-      std::add_pointer<std::tuple<__m256i, __m256i, std::size_t, std::size_t>(
-          __m256i)>::type;
-  monkey_ptr monkey0 = reinterpret_cast<monkey_ptr>(dlsym(monkey_lib, "_Z7Monkey0Dv4_x"));
+  dlerror();
+  void* monkey_lib = dlopen("./libmonkeys.so", RTLD_NOW);
+  if (auto err = dlerror(); err) {
+    std::cout << err << '\n';
+    return 1;
+  }
+  for (auto [id, monkey] : ranges::view::enumerate(monkeys)) {
+    if (monkey_ptr fn = reinterpret_cast<monkey_ptr>(
+            dlsym(monkey_lib, fmt::format("_Z7Monkey{}Dv4_x", id).c_str()));
+        fn == nullptr) {
+      if (auto err = dlerror(); err) {
+        std::cout << err << '\n';
+      }
+      std::cout << "PANIC\n";
+    } else {
+      if (auto err = dlerror(); err) {
+        std::cout << err << '\n';
+      }
+      monkey.oper = fn;
+    }
+  }
+
+  print_monkeys(monkeys);
+
+  for (auto [i, m] : ranges::view::enumerate(monkeys)) {
+    m.perform_oper(monkeys);
+    // fmt::print("after monkey {:-^80}\n", i);
+    // print_monkeys(monkeys);
+  }
+  fmt::print("after round {:=^80}\n", 1);
+  print_monkeys(monkeys);
+  for (int r = 2; r <= 20; ++r) {
+    for (auto [i, m] : ranges::view::enumerate(monkeys)) {
+      m.perform_oper(monkeys);
+      if (dbg) {
+        fmt::print("after monkey {:-^80}\n", i);
+        print_monkeys(monkeys);
+      }
+    }
+    fmt::print("after round {:=^80}\n", r);
+    print_monkeys(monkeys);
+    // if (r == 4) {
+    //   dbg = true;
+    // } else if (r == 6) {
+    //   dbg = false;
+    // }
+  }
 
   return 0;
 }
--- 11/c++-jit/monkey.h
+++ 11/c++-jit/monkey.h
@@ -2,7 +2,11 @@
 #include <Vc/Vc>
 #include <bit>
 #include <cstddef>
+#include <fmt/core.h>
+#include <fmt/format.h>
 #include <immintrin.h>
+#include <range/v3/view/enumerate.hpp>
+#include <sstream>
 #include <vector>
 #include "blend1.h"
 #include "blend2.h"
@@ -11,14 +15,46 @@
 #include "/home/pseyfert/coding/simdprune/simdprune_tables.h"
 #pragma GCC diagnostic pop
 
+using monkey_ptr =
+    std::add_pointer<std::tuple<__m256i, __m256i, std::size_t, std::size_t>(
+        __m256i)>::type;
+
+bool dbg{false};
+
 struct Monkey {
   std::vector<__m256i> items;
-  int actual_size;
+  int actual_size{0};
+
+  monkey_ptr oper{nullptr};
+  int inspections{0};
+
+  void perform_oper(std::vector<Monkey>& monkeys) {
+    for (auto [i, item] : ranges::view::enumerate(items)) {
+      /*std::tuple<__m256i, __m256i, std::size_t, std::size_t>*/ auto
+          [vals, select, true_target, false_target] = oper(item);
+      Vc::AVX2::int_v select_true =
+          (Vc::AVX2::int_v{select} && Vc::AVX2::int_v(loop_mask(i))).dataI();
+      // TODO: there's gotta be a better cast than !!
+      Vc::AVX2::int_v select_false =
+          ((!Vc::AVX2::int_v{select}) && (!!Vc::AVX2::int_v(loop_mask(i))))
+              .dataI();
+      monkeys[true_target].my_compress_store(vals, select_true.data());
+      monkeys[false_target].my_compress_store(vals, select_false.data());
+    }
+    items.clear();
+    inspections += actual_size;
+    actual_size = 0;
+  }
 
   void my_compress_store(const __m256i& other, const __m256i& mask) {
     uint8_t mask_i = _mm256_movemask_ps(_mm256_castsi256_ps(mask));
 
     auto to_insert = std::popcount(mask_i);
+
+    // TODO: shouldn't the code be okay with that?
+    // segfaulted, and debugger said that was the value of to_insert.
+    if (to_insert == 0)
+      return;
     int space_left;
     if (actual_size == 0) {
       space_left = 0;
@@ -28,32 +64,133 @@ struct Monkey {
     auto offset = (8 * (mask_i ^ 0xFF) - (actual_size % 8));
     const __m256i* lookups;
     if (offset < 0) {
+      std::cout << "WATCH OUT!\n";
       // can use mask_i ^ 0x80 instead if space_left != 0
-      std::cout << "PANIC\n";
-      reinterpret_cast<const __m256i*>(mask256_epi32);
+      // intentional shadowing
+      auto offset = (8 * (mask_i ^ 0x7F) - (actual_size % 8));
+      lookups = reinterpret_cast<const __m256i*>(mask256_epi32 + offset);
     } else {
       lookups = reinterpret_cast<const __m256i*>(mask256_epi32 + offset);
     }
-    auto idxs = _mm256_load_si256(lookups);
+    auto idxs = _mm256_loadu_si256(lookups);
+
+    if (dbg) {
+      fmt::print("offset = {}, mask_i = {}", offset, mask_i);
+      std::cout << "\nidxs = " << Vc::AVX2::int_v(idxs);
+    }
 
     __m256i compressed_buffer;
     if (space_left > 0) {
       compressed_buffer = _mm256_permutevar8x32_epi32(other, idxs);
+      if (dbg) {
+        std::cout << "\ncmpb = " << Vc::AVX2::int_v(compressed_buffer) << '\n';
+        std::cout << "other = " << Vc::AVX2::int_v(other)
+                  << "\nllm = " << Vc::AVX2::int_v(last_loop_mask()) << '\n';
+        std::cout << "back(o) = " << Vc::AVX2::int_v(items.back());
+      }
       items.back() =
-          blendv_epi32_f(compressed_buffer, items.back(), last_loop_mask());
+          blendv_epi32_pp(compressed_buffer, items.back(), last_loop_mask());
+      if (dbg) {
+        std::cout << "\nback(n) = " << Vc::AVX2::int_v(items.back()) << '\n';
+      }
     }
 
     if (to_insert > space_left) {
-      auto idxs2 = _mm256_load_si256(lookups + 1);
+      // Restore the stupid offset < 0 handling above
+      lookups = reinterpret_cast<const __m256i*>(mask256_epi32 + offset);
+      __m256i idxs2;
+      if (space_left > 0) {
+        idxs2 = _mm256_loadu_si256(lookups + 1);
+      } else {
+        idxs2 = _mm256_loadu_si256(lookups);
+      }
 
       __m256i remainders = _mm256_permutevar8x32_epi32(other, idxs2);
+      if (dbg) {
+        std::cout << "idxs2 = " << Vc::AVX2::int_v(idxs2)
+                  << "\nother = " << Vc::AVX2::int_v(other)
+                  << "\nrem = " << Vc::AVX2::int_v(remainders) << '\n';
+      }
       items.push_back(remainders);
+      if (dbg) {
+        std::cout << "back(n) = " << Vc::AVX2::int_v(items.back()) << '\n';
+      }
     }
     actual_size += to_insert;
   }
 
+  __m256i loop_mask(int i) {
+    if (i == items.size() - 1) {
+      return last_loop_mask();
+    } else {
+      return Vc::AVX2::int_v(0xffffffff).data();
+    }
+  }
+
   __m256i last_loop_mask() {
     std::array<int, 8> a{0, 1, 2, 3, 4, 5, 6, 7};
-    return (Vc::AVX2::int_v{a.data()} < (actual_size % 8)).data();
+    return (Vc::AVX2::int_v{a.data()} < (1 + ((actual_size - 1) % 8))).data();
   }
 };
+
+template <>
+struct fmt::formatter<Monkey> {
+  // inspired by https://fmt.dev/latest/api.html#formatting-user-defined-types
+  constexpr auto parse(fmt::format_parse_context& ctx) {
+    auto it = ctx.begin();
+    auto end = ctx.end();
+    if (it != end) {
+      it++;
+      mid = *it++;
+    }
+
+    // Check if reached the end of the range:
+    if (it != end && *it != '}')
+      throw fmt::format_error("invalid format");
+
+    // Return an iterator past the end of the parsed range:
+    return it;
+  }
+
+  char mid = '_';
+
+  template <typename FormatContext>
+  constexpr auto format(Monkey const& m, FormatContext& ctx) const {
+    std::ostringstream ss;
+    for (auto& item : m.items) {
+      ss << Vc::AVX2::int_v(item) << ',';
+    }
+    ss << "\t(" << m.actual_size << ')';
+    ss << "\t activity: " << m.inspections;
+    if (mid == '_')
+      return fmt::format_to(ctx.out(), "Monkey: {}", ss.str());
+    else
+      return fmt::format_to(ctx.out(), "Monkey {}: {}", mid, ss.str());
+  }
+};
+
+void print_monkeys(std::vector<Monkey> const& monkeys) {
+  for (auto [i, m] : ranges::view::enumerate(monkeys)) {
+    if (m.oper == nullptr)
+      std::cout << "PANIC\n";
+    if (i == 0) {
+      fmt::print("{:m0}\n", m);
+    } else if (i == 1) {
+      fmt::print("{:m1}\n", m);
+    } else if (i == 2) {
+      fmt::print("{:m2}\n", m);
+    } else if (i == 3) {
+      fmt::print("{:m3}\n", m);
+    } else if (i == 4) {
+      fmt::print("{:m4}\n", m);
+    } else if (i == 5) {
+      fmt::print("{:m5}\n", m);
+    } else if (i == 6) {
+      fmt::print("{:m6}\n", m);
+    } else if (i == 7) {
+      fmt::print("{:m7}\n", m);
+    } else {
+      fmt::print("{}\n", m);
+    }
+  }
+}
