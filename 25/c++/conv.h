23becc4e0ca93faf795ecb7695a6e373becb1ba5
Paul Seyfert <pseyfert.mathphys@gmail.com>
2022-12-27 21:28:22 +0100

    writing snafu

--- 25/c++/conv.h
+++ 25/c++/conv.h
@@ -2,7 +2,6 @@
 #include <numeric>
 #include <string_view>
 
-// TODO
 unsigned long fromSNAFU(std::string_view s) {
   return std::accumulate(
       s.begin(), s.end(), 0ul, [](unsigned long acc, const char c) {
@@ -30,4 +29,41 @@ unsigned long fromSNAFU(std::string_view s) {
         // comment to prevent clang format from merging lines
         return acc;
       });
+}
+
+std::string toSNAFU(unsigned long s) {
+  std::string reverse_retval;
+  for (; s != 0;) {
+    // alternative:
+    // tmp = std::div(s, 5);
+    // s = tmp.quot;
+    // if (tmp.rem > 2) s++;
+    switch (s % 5) {
+      case 2:
+        reverse_retval += '2';
+        s -= 2;
+        break;
+      case 1:
+        reverse_retval += '1';
+        s -= 1;
+        break;
+      case 0:
+        reverse_retval += '0';
+        s -= 0;
+        break;
+      case 3:
+        reverse_retval += '=';
+        s += 2;
+        break;
+      case 4:
+        reverse_retval += '-';
+        s += 1;
+        break;
+    }
+    if (s % 5) {
+      throw std::logic_error("my math is wrong");
+    }
+    s /= 5;
+  }
+  return std::string(reverse_retval.rbegin(), reverse_retval.rend());
 }
--- 25/c++/read_test.cpp
+++ 25/c++/read_test.cpp
@@ -2,17 +2,37 @@
 #include "conv.h"
 
 TEST(parse, example) {
+  // clang-format off
   EXPECT_EQ(fromSNAFU("1=-0-2"), 1747ul);
-  EXPECT_EQ(fromSNAFU("12111"), 906ul);
-  EXPECT_EQ(fromSNAFU("2=0="), 198ul);
-  EXPECT_EQ(fromSNAFU("21"), 11ul);
-  EXPECT_EQ(fromSNAFU("2=01"), 201ul);
-  EXPECT_EQ(fromSNAFU("111"), 31ul);
-  EXPECT_EQ(fromSNAFU("20012"), 1257ul);
-  EXPECT_EQ(fromSNAFU("112"), 32ul);
-  EXPECT_EQ(fromSNAFU("1=-1="), 353ul);
-  EXPECT_EQ(fromSNAFU("1-12"), 107ul);
-  EXPECT_EQ(fromSNAFU("12"), 7ul);
-  EXPECT_EQ(fromSNAFU("1="), 3ul);
-  EXPECT_EQ(fromSNAFU("122"), 37ul);
+  EXPECT_EQ(fromSNAFU("12111" ),  906ul);
+  EXPECT_EQ(fromSNAFU("2=0="  ),  198ul);
+  EXPECT_EQ(fromSNAFU("21"    ),   11ul);
+  EXPECT_EQ(fromSNAFU("2=01"  ),  201ul);
+  EXPECT_EQ(fromSNAFU("111"   ),   31ul);
+  EXPECT_EQ(fromSNAFU("20012" ), 1257ul);
+  EXPECT_EQ(fromSNAFU("112"   ),   32ul);
+  EXPECT_EQ(fromSNAFU("1=-1=" ),  353ul);
+  EXPECT_EQ(fromSNAFU("1-12"  ),  107ul);
+  EXPECT_EQ(fromSNAFU("12"    ),    7ul);
+  EXPECT_EQ(fromSNAFU("1="    ),    3ul);
+  EXPECT_EQ(fromSNAFU("122"   ),   37ul);
+  // clang-format on
+}
+
+TEST(write, example) {
+  // clang-format off
+  EXPECT_EQ("1=-0-2", toSNAFU(1747ul));
+  EXPECT_EQ("12111" , toSNAFU( 906ul));
+  EXPECT_EQ("2=0="  , toSNAFU( 198ul));
+  EXPECT_EQ("21"    , toSNAFU(  11ul));
+  EXPECT_EQ("2=01"  , toSNAFU( 201ul));
+  EXPECT_EQ("111"   , toSNAFU(  31ul));
+  EXPECT_EQ("20012" , toSNAFU(1257ul));
+  EXPECT_EQ("112"   , toSNAFU(  32ul));
+  EXPECT_EQ("1=-1=" , toSNAFU( 353ul));
+  EXPECT_EQ("1-12"  , toSNAFU( 107ul));
+  EXPECT_EQ("12"    , toSNAFU(   7ul));
+  EXPECT_EQ("1="    , toSNAFU(   3ul));
+  EXPECT_EQ("122"   , toSNAFU(  37ul));
+  // clang-format on
 }
